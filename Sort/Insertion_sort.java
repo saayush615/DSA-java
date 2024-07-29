f4018dff8445cd6b144a2b33013d30c2553ca282
Aayush singh <saayush615@gmail.com>
2024-07-29 09:04:00 +0530

    Added the code for insertion sort

--- Sort/Insertion_sort.java
+++ Sort/Insertion_sort.java
@@ -1,4 +1,33 @@
 package DSA.Sort;
 
+import java.util.Arrays;
+
 public class Insertion_sort {
+    public static void main(String[] args) {
+        int[] arr = {5,4,3,1,2,6};
+        insertion(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void insertion(int[] arr){
+        // outer loop
+        for (int i = 0; i < arr.length - 1; i++) {
+            // inner loop
+            for (int j = i+1; j > 0; j--) {
+//                check if prev ele is greater than this elment if yes then swap
+                if (arr[j] < arr[j-1]) {
+                    swap(arr,j,j-1);
+                }
+                else{
+                    break;
+                }
+            }
+        }
+    }
+
+    static void swap(int[] arr,int second,int first){
+        int temp = arr[second];
+        arr[second] = arr[first];
+        arr[first] = temp;
+    }
 }

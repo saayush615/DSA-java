36034e327f95d782cddd68577c086ab9eaf1c557
Aayush singh <saayush615@gmail.com>
2024-08-08 09:06:14 +0530

    modified bitwise1

--- Math_dsa/Bitwise1.java
+++ Math_dsa/Bitwise1.java
@@ -1,4 +1,33 @@
 package DSA.Math_dsa;
 
+
 public class Bitwise1 {
+    public static void main(String[] args) {
+//        System.out.println(10 << 1);
+//        int n =15;
+//        System.out.println(10>n? "yes" : "no");
+
+//        q1
+//        int n = 67;
+//        System.out.println(isOdd(n));
+
+//        q2
+        int[] arr = {2,3,3,4,2,6,4};
+        System.out.println(ans(arr));
+    }
+
+    //q1
+//    private static boolean isOdd(int n) {
+//        return (n&1) == 1;
+//    }
+
+    //q2
+    private static int ans(int[] arr) {
+        int unique = 0;
+//        just xor every no.
+        for (int n : arr){
+            unique ^= n;
+        }
+        return unique;
+    }
 }

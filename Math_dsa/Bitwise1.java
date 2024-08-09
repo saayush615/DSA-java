684a1d1a1912b560b0ce76f7030de0d682b8bf17
Aayush singh <saayush615@gmail.com>
2024-08-09 08:24:29 +0530

    complited with bitwise1

--- Math_dsa/Bitwise1.java
+++ Math_dsa/Bitwise1.java
@@ -12,9 +12,28 @@ public class Bitwise1 {
 //        System.out.println(isOdd(n));
 
 //        q2
-        int[] arr = {2,3,3,4,2,6,4};
-        System.out.println(ans(arr));
+//        int[] arr = {2,3,3,4,2,6,4};
+//        System.out.println(ans(arr));
+
+//        q4: find i'th bit : count from backwards
+//        int num = 19;
+//        System.out.println(Integer.toBinaryString(num));
+//        int position = 2;
+//        System.out.println(find_bit(num, position));
+
+//        q5:set
+//        int num = 19;
+//        System.out.println(Integer.toBinaryString(num));
+//        int position = 4;
+//        System.out.println(Integer.toBinaryString(set_bit(num, position)));
+
+//        q6:reset
+//        int num = 19;
+//        System.out.println(Integer.toBinaryString(num));
+//        int position = 2;
+//        System.out.println(Integer.toBinaryString(reset_bit(num, position)));
     }
+
 
     //q1
 //    private static boolean isOdd(int n) {
@@ -22,12 +41,29 @@ public class Bitwise1 {
 //    }
 
     //q2
-    private static int ans(int[] arr) {
-        int unique = 0;
-//        just xor every no.
-        for (int n : arr){
-            unique ^= n;
-        }
-        return unique;
-    }
+//    private static int ans(int[] arr) {
+//        int unique = 0;
+////        just xor every no.
+//        for (int n : arr){
+//            unique ^= n;
+//        }
+//        return unique;
+//    }
+
+    //q4
+//    private static int find_bit(int num, int pos) {
+//        return (num & (1 << (pos - 1))) >> (pos - 1);
+//        // or
+////        return (num >> (pos -1)) & (pos -1);
+//    }
+
+    // q5 :set
+//    private static int set_bit(int num, int position) {
+//        return num | (1<<(position - 1));
+//    }
+
+//    q6 : reset
+//    private static int reset_bit(int num, int pos) {
+//        return num & ~(1 << (pos-1));
+//    }
 }

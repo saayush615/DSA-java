e1544093c4eb62ffe1d4c606affd29a966c85f71
Aayush singh <saayush615@gmail.com>
2024-08-13 06:07:56 +0530

    added Bitwise2.java

--- Math_dsa/Bitwise2.java
+++ Math_dsa/Bitwise2.java
@@ -1,4 +1,20 @@
 package DSA.Math_dsa;
 
 public class Bitwise2 {
+    public static void main(String[] args) {
+//        q7 : to do negetaive first do compliment and then add 1 to it
+        int[] arr = {2, 2, 3, 2, 7, 7, 8, 7, 8, 8};
+        System.out.println(times(arr));
+    }
+
+    private static int times(int[] arr) {
+        int sum = 0;
+        for(int a : arr){
+            sum += a;
+        }
+        if (sum % 3 == 0){
+            return -1;
+        }
+        return sum%3;
+    }
 }
